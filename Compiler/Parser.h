ebff3c3383d4d756a40b7b78e65956566b98e2eb
hyst329 <trolleybus.1329@gmail.com>
2014-10-02 10:20:27 +0400

    Added TokenType

--- Compiler/Parser.h
+++ Compiler/Parser.h
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <boost/spirit/include/qi.hpp>
 
 using namespace std;
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::qi::ascii;
+
 namespace F4 {
     enum TokenType {
-
+        // Identifiers and literals
+                TT_IDENT,
+        TT_INTLITERAL,
+        TT_DOUBLELITERAL,
+        TT_STRINGLITERAL, // TODO String types
+        // Punctuation
+                TT_LPAREN,
+        TT_RPAREN,
+        TT_LBRACKET,
+        TT_RBRACKET,
+        TT_COLON,
+        // Operators
+                TT_ASSIGN,
+        // Keywords
+                TT_KW_IF,
+        TT_KW_ENDIF,
+        TT_KW_LOOP,
+        TT_KW_ENDLOOP,
+        // Total count
+                TT_COUNT
     };
 
     struct Token {
@@ -16,6 +39,7 @@ namespace F4 {
     class Parser {
     private:
         ifstream *ifile;
+        qi::rule<string::iterator, string(), ascii::space_type> rules[TT_COUNT];
     public:
         Parser(ifstream *ifile);
         vector<Token> parse();
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-f4-programming-language
+f4 Programming Language
 =======================
 
 Simple project of an educational programming language. The implementation is in the planning stage.
+
+
+
+Official site (stub): http://f4pl.tk
