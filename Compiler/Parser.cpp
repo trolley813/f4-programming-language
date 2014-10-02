15bac43d2a1190e9be53345f816e575f042d5ab8
hyst329 <trolleybus.1329@gmail.com>
2014-10-02 15:37:51 +0400

    Added some TokenType values

--- Compiler/Parser.cpp
+++ Compiler/Parser.cpp
@@ -1,7 +1,19 @@
 #include "Parser.h"
 
+namespace bs = boost::spirit
+namespace qi = boost::spirit::qi;
+
 namespace F4 {
     Parser::Parser(ifstream *ifile) : ifile(ifile) {
+        q[TT_IDENT] = bs::lexeme[qi::alpha >> *qi::alnum];
+        q[TT_INTLITERAL] = bs::lexeme[+qi::digit];
+        q[TT_REALLITERAL] = bs::lexeme[(-(qi::char_('+') | qi::char_('-')) >> +qi::digit) >> ((qi::char_(',') | qi::char_('.')) >> +qi::digit)];
+        q[TT_LPAREN] = qi::char_('(');
+        q[TT_RPAREN] = qi::char_(')');
+        q[TT_LBRACKET] = qi::char_('[');
+        q[TT_RBRACKET] = qi::char_(']');
+        q[TT_COLON] = qi::char_(':');
+///     q[TT_ARROW] = qi::string("->");
     }
     
     vector<Token> Parser::parse() {
--- Compiler/Parser.h
+++ Compiler/Parser.h
@@ -10,25 +10,47 @@ namespace ascii = boost::spirit::qi::ascii;
 namespace F4 {
     enum TokenType {
         // Identifiers and literals
-                TT_IDENT,
+        TT_IDENT,
         TT_INTLITERAL,
-        TT_DOUBLELITERAL,
+        TT_REALLITERAL,
         TT_STRINGLITERAL, // TODO String types
         // Punctuation
-                TT_LPAREN,
-        TT_RPAREN,
-        TT_LBRACKET,
-        TT_RBRACKET,
-        TT_COLON,
+        TT_LPAREN,     // (
+        TT_RPAREN,     // )
+        TT_LBRACKET,   // [
+        TT_RBRACKET,   // ]
+        TT_COLON,      // :
+        TT_ARROW,      // ->
         // Operators
-                TT_ASSIGN,
+        TT_ASSIGN,     // =
+        TT_PLUS,       // +
+        TT_MINUS,      // -
+        TT_MULTIPLY,   // *
+        TT_DIVIDE,     // /
+        TT_AND,        // &
+        TT_OR,         // |
+        TT_NOT,        // !
+        TT_ASPLUS,     // +=
+        TT_ASMINUS,    // -=
+        TT_ASMULT,     // *=
+        TT_ASDIV,      // /=
+        TT_ASAND,      // &=
+        TT_ASOR,       // |=
+        TT_UNARYPLUS,  // +
+        TT_UNARYMINUS  // -
         // Keywords
-                TT_KW_IF,
+        TT_KW_IF,
         TT_KW_ENDIF,
         TT_KW_LOOP,
         TT_KW_ENDLOOP,
+        TT_KW_VAR,
+        TT_KW_INT,
+        TT_KW_REAL,
+        TT_KW_LOGICAL,
+        TT_KW_FUN,
+        TT_KW_ENDFUN,
         // Total count
-                TT_COUNT
+        TT_COUNT
     };
 
     struct Token {
